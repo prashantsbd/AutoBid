0a20f461b1478debe7dca289c98ba42c33cc7d81
prashantcode79 <9846064078Z@gmail.com>
2023-08-01 14:01:57 +0545

    geniune check

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello world")
+try:
+    import selenium
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+except:
+    print("importer has error")
+
+try:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+except:
+    print("driver has error")
