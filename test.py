b99d056a152cca22889162ec83e08b29e91a0876
prashantcode79 <9846064078Z@gmail.com>
2023-08-01 14:07:22 +0545

    geniune check

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ try:
     from selenium import webdriver
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
+    import chromedriver_autoinstaller
 except:
     print("importer has error")
 
 try:
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chromedriver_autoinstaller.install()
 except:
-    print("driver has error")
+    print("driver was now found by this route")
