6a5e236613c8cd0b809a380fbc386ca440ad447d
prashantcode79 <9846064078Z@gmail.com>
2023-08-02 21:18:02 +0545

    chrome version bug

--- test.py
+++ test.py
@@ -15,17 +15,17 @@ except:
 
 
 try:
-    # option = webdriver.ChromeOptions()
-    # option.add_argument("--window-size=1180,650")
-    # option.add_argument("--headless")
-    # option.add_argument("--disable-notification")
-    # option.add_argument("--disable-blink-features=AutomationControlled")
-    # option.add_argument("--disable-gpu")
-    # option.add_argument("--disable-webgl")
-    # driver = webdriver.Chrome(options=option)
-    display = Display(visible=0, size=(800, 800))
-    display.start()
-    driver = webdriver.Chrome()
+    option = webdriver.ChromeOptions()
+    option.add_argument("--window-size=1180,650")
+    option.add_argument("--headless")
+    option.add_argument("--disable-notification")
+    option.add_argument("--disable-blink-features=AutomationControlled")
+    option.add_argument("--disable-gpu")
+    option.add_argument("--disable-webgl")
+    driver = webdriver.Chrome(options=option)
+    # display = Display(visible=0, size=(800, 800))
+    # display.start()
+    # driver = webdriver.Chrome()
 
 except:
     print("driver pet was not lifted")
