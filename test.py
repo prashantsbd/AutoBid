71b8c97e82e766ee4901a5325e2c0a9624632b6f
prashantcode79 <9846064078Z@gmail.com>
2023-08-02 21:38:41 +0545

    check if virtual display

--- test.py
+++ test.py
@@ -23,6 +23,7 @@ try:
     option.add_argument("--disable-gpu")
     option.add_argument("--disable-webgl")
     driver = webdriver.Chrome(options=option)
+    # new push
     # display = Display(visible=0, size=(800, 800))
     # display.start()
     # driver = webdriver.Chrome()
