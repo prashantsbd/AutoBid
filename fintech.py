9486787975446e20867c1b54120244d9018cb448
prashantcode79 <9846064078Z@gmail.com>
2025-09-17 22:22:14 +0545

    try anti bot protection

--- fintech.py
+++ fintech.py
@@ -9,6 +9,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
+import undetected_chromedriver as uc
+driver = uc.Chrome()
+
 
 
 def envcall(array):
